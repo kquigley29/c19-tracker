e2e557294106d1840677cb8ed0b82e70004214dc
Keane Quigley <keaneq@protonmail.com>
2020-04-21 05:09:35 +0100

    add required function to display in html format

--- convert.py
+++ convert.py
@@ -13,3 +13,19 @@ def csv_to_dict(file):
     for line in reader:
         dict_list.append(line)
     return dict_list
+
+
+def dict_to_html(input_dict):
+    table_head = '<tr>'
+    for key in input_dict[0].keys():
+        table_head += '<th>' + str(key) + '</th>'
+    table_head += '</tr>'
+
+    table_data = ''               
+    for row_dict in input_dict:
+        table_data += '<tr>'
+        for key in row_dict.keys():
+            table_data += "<td>"  + str(row_dict[key]) + "</td>"
+        table_data += '</tr>'
+
+    return "<table border=1>" + table_head + table_data + "</table>"
