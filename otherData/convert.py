c06a7f346ac2bd83e67f62cd321cc827ea3f9a0c
keane quigley <keaneq@example.com>
2020-08-04 00:10:31 +0100

    remove unused scripts

--- otherData/convert.py
+++
@@ -1,9 +0,0 @@
-from csv import DictReader
-
-
-def csv_to_dict(file):
-    reader = DictReader(open(file))
-    dict_list = []
-    for line in reader:
-        dict_list.append(line)
-    return dict_list
